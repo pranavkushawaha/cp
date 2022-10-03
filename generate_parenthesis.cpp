146629bd099357d767abaf739f09e48d3b7ae6f2
aric-ash72 <114927314+aric-ash72@users.noreply.github.com>
2022-10-03 18:45:41 +0530

    generate parenthesis

---
+++ generate_parenthesis.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<string> valid;
+
+void generate(string &s, int open ,int close){
+    if(open==0 and close==0){
+        valid.push_back(s);
+        return;
+    }
+    if(open>0){
+        s.push_back('(');
+        generate(s,open-1,close);
+        s.pop_back();
+    }
+    if(close>0){
+        if(open<close){
+            s.push_back(')');
+            generate(s,open,close-1);
+            s.pop_back();
+        }
+    }
+}
+
+int main(){
+    int num;
+    cin>>num;
+    string s;
+    generate(s,num,num);
+    for(auto ele: valid){
+        cout<<ele<<endl;
+    }
+    return 0;
+}
